1c2c3a95ab88ebc28c8c9061956d2d99860e255e
PsyxAI <psyxartificialintelligence@gmail.com>
2018-06-05 14:31:02 -0700

    Develpoment N4 Removed unnecessary files Added a few more extension methods

--- PsyxCalc/PsyxCalc/Resources/Calc/Percent.cs
+++ PsyxCalc/PsyxCalc/Resources/Calc/Percent.cs
@@ -2,8 +2,8 @@
 {
     public static partial class Calc
     {
-        public static double Percent(double x, double y) => ((x / y) * (double)100);
-        public static float Percent(float x, float Y) => (x / y) * 100;
+        public static double Percent(double x, double y) => ((x / y) * 100);
+        public static float Percent(float x, float y) => (x / y) * 100;
         public static int Percent(int x, int y) => (x / y) * 100;
         public static long Percent(long x, long y) => (x / y) * 100;
         public static short Percent(short x, short y) => (short)((x / y) * 100);
--- PsyxCalc/PsyxCalc/Resources/Extra/ExtensionMethods.cs
+++ PsyxCalc/PsyxCalc/Resources/Extra/ExtensionMethods.cs
@@ -3,8 +3,27 @@
     public static class ExtensionMethods
     {
 
+        #region Round
         public static int Round(this int x, int roundTo) => PsyxCalc.Calc.Round(x, roundTo);
         public static int Round(this int x) => PsyxCalc.Calc.Round(x);
+
+        public static long Round(this long x, long roundTo) => PsyxCalc.Calc.Round(x, roundTo);
+        public static long Round(this long x) => PsyxCalc.Calc.Round(x);
+
+        public static byte Round(this byte x, byte roundTo) => PsyxCalc.Calc.Round(x, roundTo);
+        public static byte Round(this byte x) => PsyxCalc.Calc.Round(x);
+
+        public static double Round(this double x, double roundTo) => PsyxCalc.Calc.Round(x, roundTo);
+        public static double Round(this double x) => PsyxCalc.Calc.Round(x);
+
+        public static float Round(this float x, float roundTo) => PsyxCalc.Calc.Round(x, roundTo);
+        public static float Round(this float x) => PsyxCalc.Calc.Round(x);
+
+        public static decimal Round(this decimal x, decimal roundTo) => PsyxCalc.Calc.Round(x, roundTo);
+        public static decimal Round(this decimal x) => PsyxCalc.Calc.Round(x);
+        #endregion Round
+
+
         //TODO
     }
 }
